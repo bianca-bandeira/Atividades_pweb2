11ddd4da57ee43cad92519639d3ad0d4c6be5501
pprtbia <biancarb1421@gmail.com>
2025-09-26 10:20:59 -0300

    Questões do dia:24/09/25

---
+++ ex01.js
@@ -0,0 +1,4 @@
+import {area} from "./esm-named.js";
+
+console.log(area(10))
+console.log(area(1))
---
+++ ex02.js
@@ -0,0 +1,6 @@
+import {calculadora} from "./esm-named.js"
+
+console.log(calculadora(1,1,"+"))
+console.log(calculadora(4,1,"-"))
+console.log(calculadora(2,8,"*"))
+console.log(calculadora(16,1,"/"))
